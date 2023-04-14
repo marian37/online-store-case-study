20cbc6d947782a2a93b1f24471593b52f4bd5fba
marian37 <opiela.marian@gmail.com>
2023-04-14 20:58:38 +0200

    Remove unused code, prepare routes structure

---
+++ src/app/Home.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/products">Start shopping</Link>
+    </div>
+  );
+};
+
+export default Home;
